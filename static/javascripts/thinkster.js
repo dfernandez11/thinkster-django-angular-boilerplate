867f91d1649f10a28e1c912ac5eec1d93a7911d6
dfernandez11 <dfernandez1110@gmail.com>
2017-01-05 18:12:10 -0300

    Registering new users complete

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -4,27 +4,30 @@
   angular
     .module('thinkster', [
       'thinkster.routes',
-      'thinkster.authentication'
-      'thinkster.config',
+      'thinkster.authentication',
+      'thinkster.config'
     ]);
 
   angular
     .module('thinkster.routes', ['ngRoute']);
-    .module('thinkster.config', []);
 
-})();
+   angular
+  .module('thinkster.config', []);
 
-angular
+  angular
   .module('thinkster')
   .run(run);
 
-run.$inject = ['$http'];
+    run.$inject = ['$http'];
 
-/**
-* @name run
-* @desc Update xsrf $http headers to align with Django's defaults
-*/
-function run($http) {
-  $http.defaults.xsrfHeaderName = 'X-CSRFToken';
-  $http.defaults.xsrfCookieName = 'csrftoken';
-}
+    /**
+    * @name run
+    * @desc Update xsrf $http headers to align with Django's defaults
+    */
+    function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+    }
+
+})();
+
