edceceb43713707e0580534d3578bac59fbf39aa
dfernandez11 <dfernandez1110@gmail.com>
2017-01-09 16:56:34 -0300

    making post object complete

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 from django.conf.urls import include
 from thinkster_django_angular_boilerplate.views import IndexView
 from authentication.views import LogoutView
+from posts.views import AccountPostsViewSet, PostViewSet
+
 
 from rest_framework_nested import routers
 
@@ -11,9 +13,18 @@ from authentication.views import LoginView
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
+router.register(r'posts', PostViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+
+accounts_router.register(r'posts', AccountPostsViewSet)
+
 urlpatterns = patterns(
     '',
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url('^.*$', IndexView.as_view(), name='index'),
